274298a452f371ba0be164a8556e1a2ff4fbcb42
Anubhav Saini <iamanubhavsaini@gmail.com>
2017-01-05 11:24:51 +0530

    Working makendir

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const exec = require('child_process').exec
+const prompt = require('prompt')
+
+const mkdir = 'mkdir'
+const combine = '&&'
+const enter = 'cd'
+
+const runMaken = dir => {
+    exec(`${mkdir} ${dir} ${combine} ${enter} ${dir}`, (err, stdout, stderr) => {
+        if (err) {
+            console.log(stderr)
+        }
+    })
+}
+
+prompt.start()
+prompt.get(['directory'], (err, res) => {
+    if (err) {
+        console.log(err)
+    }
+    runMaken(res.directory)
+})
